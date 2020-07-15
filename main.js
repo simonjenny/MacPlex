25b43c5adb25a333dde4603f92c913ccdc387725
Simon Jenny <simon.jenny@me.com>
2020-07-15 09:42:37 +0200

    Updates

--- main.js
+++ main.js
@@ -36,13 +36,7 @@ function createWindow() {
 
   var template = [{
       label: "Application",
-      submenu: [{
-          label: "About Application",
-          selector: "orderFrontStandardAboutPanel:"
-        },
-        {
-          type: "separator"
-        },
+      submenu: [
         {
           label: "Quit",
           accelerator: "Command+Q",
@@ -52,6 +46,19 @@ function createWindow() {
         }
       ]
     },
+    {
+      label: "Edit",
+      submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]
+    },
+
     {
       label: 'View',
       submenu: [{
